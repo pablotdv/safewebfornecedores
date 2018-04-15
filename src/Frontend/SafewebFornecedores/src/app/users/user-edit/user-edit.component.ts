abda0575833c1c8a27728cbbd8b5bb39437ee723
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2018-04-14 21:25:13 -0300

    RF01 - Gerenciar o cadastro de usuário

--- src/Frontend/SafewebFornecedores/src/app/users/user-edit/user-edit.component.ts
+++ src/Frontend/SafewebFornecedores/src/app/users/user-edit/user-edit.component.ts
@@ -12,6 +12,7 @@ import { DatePipe } from '@angular/common';
 })
 export class UserEditComponent implements OnInit {
   mask = [/[0-9]/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '-', /\d/, /\d/];
+  maskDate = [/[0-9]/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/];
 
   usuarioForm: FormGroup;
 
@@ -21,13 +22,10 @@ export class UserEditComponent implements OnInit {
     private fb: FormBuilder) {
   }
 
-  usuario: Usuario;
-
   ngOnInit() {
     this.usersService.get(this.route.snapshot.params['id'])
       .subscribe(usuario => {
-        this.usuario = usuario;
-        this.createForm(this.usuario);
+        this.createForm(usuario);
       });
   }
 
@@ -35,7 +33,7 @@ export class UserEditComponent implements OnInit {
     this.usuarioForm = this.fb.group({
       cpf: [value.Cpf, [Validators.required]],
       nome: [value.Nome, [Validators.required]],
-      dataNascimento: [new Date(value.DataNascimento), [Validators.required]],
+      dataNascimento: ['12/12/1986', [Validators.required]],
       email: [value.Email, [Validators.required]],
       id: [value.Id],
       userName: [value.UserName, [Validators.required]]
@@ -49,8 +47,22 @@ export class UserEditComponent implements OnInit {
   get id() { return this.usuarioForm.get('id'); }
   get userName() { return this.usuarioForm.get('userName'); }
 
-  teste(value) {
-    console.log(new Date(value.value).toISOString());
+  prepareToSave(): any {
+    const formModel = this.usuarioForm.value;
+
+    const usuarioModel = {
+      Cpf: formModel.cpf,
+      Nome: formModel.nome,
+      DataNascimento: formModel.dataNascimento,
+      Email: formModel.email,      
+      Id: formModel.id,      
+    };
+    return usuarioModel;
+  }
+
+  onSubmit() {
+    let usuarioModel = this.prepareToSave();
+    console.log(usuarioModel)    
   }
 
 }
